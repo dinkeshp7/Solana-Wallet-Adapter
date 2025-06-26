00c81f46c11c49fec75c79f83e79f3c9ddb62f0f
dinkeshp7 <dinkeshgta@gmail.com>
2025-06-26 16:56:22 +0530

    some bugs fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { ExternalLink } from "lucide-react"
 export const Footer = ()=>{
   return <footer>
     <div className=" text-gray-500 fixed bottom-2 p-2 flex justify-end">
-      Made by <a target="_blank" href="https://github.com/sudheer0071" className=" hover:scale-110 transition-all duration-500 hover:text-slate-300 hover:ml-3 ml-1"> sudheer0071 <ExternalLink size={20} className={` inline  ml-1`}/> </a>
+      Made by <a target="_blank" href="https://github.com/dinkeshp7/Solana-Wallet-Adapter" className=" hover:scale-110 transition-all duration-500 hover:text-slate-300 hover:ml-3 ml-1"> dinkeshp7 <ExternalLink size={20} className={` inline  ml-1`}/> </a>
     </div>
   </footer>
 }
